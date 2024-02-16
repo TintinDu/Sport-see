73928d7d8ed79d0fca4ac8a023a2f8f6d835b895
martin.dubois <martin.dubois@foncia.com>
2024-02-16 15:33:13 +0100

    feat: init sport see project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import sportSee from "/sportSee.svg";
+import "./App.css";
+import { test } from "./api";
 
-function App() {
-  const [count, setCount] = useState(0)
+const user = await test();
 
+console.log(user);
+
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="logo">
+        <img src={sportSee} alt="Logo SportSee" />
+      </h1>
+      <h2>Bonjour {user.data.data.userInfos.firstName}</h2>
     </>
-  )
+  );
 }
-
-export default App
